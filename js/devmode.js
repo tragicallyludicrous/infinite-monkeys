2d944e3cb0432f95b02bb594a7fd29fcb24fc4c7
tragicallyludicrous <zack.dezon@gmail.com>
2026-05-06 09:09:43 -0400

    add cash button for devmode

--- js/devmode.js
+++ js/devmode.js
@@ -5,6 +5,7 @@ import { monkeyTypes } from "./config.js";
 import { camelToPascal } from "./format.js";
 import { getButton } from "./dom.js";
 import { monkeyListeners } from "./main.js";
+import { gameState } from "./state.js";
 
 // ====================================
 // --- DEV MODE ---
@@ -22,12 +23,17 @@ export function devModeActivate() {
   document.getElementById("dev-mode").style.display = "block";
   document.getElementById("dev-mode").innerHTML = `
   <h3>DEVELOPER MODE</h3>
+  <button id="dev-add-cash">Add $1,000,000</button>
   ${enumerateDevTypes()}
   <button id="dev-buy-autoClicker">Add AutoClicker</button>
   <button id="dev-speed-upgrade">Upgrade Speed (ALL)</button>
   <button id="dev-int-upgrade">Upgrade Int (ALL)</button>
   <hr>
 `;
+
+  document
+    .getElementById("dev-add-cash")
+    .addEventListener("click", () => (gameState.cash += 1000000));
 
   monkeyListeners("dev-");
 
